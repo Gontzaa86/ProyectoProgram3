85ffe576abed3369c28e5a928c3605e3750101bf
User <User@DESKTOP-1BQD9B0.home>
2023-11-27 13:25:33 +0100

    Al iniciar el menu inicial y clickar un boton el juego se ejecuta.

---
+++ Juegos/src/MenuInicial/Main.java
@@ -0,0 +1,17 @@
+package MenuInicial;
+
+import Pacman.juegoVentanaPacman;
+import Snake3.juegoMenu;
+
+public class Main 
+{
+	public void iniciarSnake()
+	{
+		new juegoMenu();
+	}
+	
+	public void iniciarPacMan()
+	{
+		new juegoVentanaPacman();
+	}
+}
--- Juegos/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module Juegos {
-	requires java.desktop;
-}
