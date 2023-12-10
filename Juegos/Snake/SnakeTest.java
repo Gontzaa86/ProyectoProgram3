ca98e91b1484bf6477ed3675840af721bc3bdd8d
User <User@DESKTOP-1BQD9B0.home>
2023-12-10 15:24:28 +0100

    JUnit Snake

---
+++ Juegos/Snake/SnakeTest.java
@@ -0,0 +1,31 @@
+package Snake;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+
+public class SnakeTest 
+{
+	@Test
+	public void pruebaSnake()
+	{
+		juegoContenido snake = new juegoContenido(150, false, false);
+		
+		//Comprobar que se genean comidas
+		snake.agregarComida();
+		int comidaX = snake.comidaX;
+		int comidaY = snake.comidaY;
+		
+		assertNotEquals(0, comidaX);
+		assertNotEquals(0, comidaY);
+		
+		//Comprobar que se crean bloques
+		snake.agregarBloque(true);
+		int[] bloqueX = snake.bloqueX;
+		int[] bloqueY = snake.bloqueY;
+		
+		assertNotEquals(0, bloqueX);
+		assertNotEquals(0, bloqueY);
+	}
+}
